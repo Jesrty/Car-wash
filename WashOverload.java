b36c4c5c15a3e5697a57db6a5b282470f6940c23
Jesrty <ravn1234567890@hotmail.com>
2018-11-08 09:30:38 +0100

    noter

--- WashOverload.java
+++ WashOverload.java
@@ -12,31 +12,38 @@ public class WashOverload{
    public void setWow(int wow){
       this.wow = wow;
    }
-
+   
+   //Gemmer helle filen customer.txt i et array af strings også ændre en balancen på en bestem linje
    public void fileWrite(File file, double balanceChange, int idid)throws FileNotFoundException{
    
       Scanner s = new Scanner(file);
-               
+      
+      //tæller antal linjer i filen customer.txt         
       while(s.hasNextLine()){
          i++;
          placeHolder = s.nextLine();
             
       }
+      //laver et array der er lige så langt som antal linjer i filen customer.txt
       String[] customerHolder = new String[i];
       s.close();
       
+      //sætter hver linje i filen customer.txt ligmed et af arraysne (så array [0] er lig med linje 1
       Scanner ss = new Scanner(file);
       for(int p = 0; p<i; p++){
          customerHolder[p] = ss.nextLine();
       }
-         
+      
+      //splitter linjen med det intastede id op i flere arrays   
       String[] customerSplit = customerHolder[idid].split(",");
-         
+      
+      //ændre balance værdien på det bestemte id   
       double[] customerChange = new double[1];
       customerChange[0] = (Double.parseDouble(customerSplit[2]) + balanceChange);
          
       customerHolder[idid] = customerSplit[0] + "," + customerSplit[1] + "," + customerChange[0];
-         
+      
+      //hvis beløbet ikke bliver negativt "sletter" den den gamle fil og skriver det ind igen med ændringen i balancen   
       if(customerChange[0]>=0){
          PrintStream outPrint = new PrintStream(file);
       
@@ -48,6 +55,7 @@ public class WashOverload{
          
          System.out.println("your balance is now: " + customerChange[0] + "\n");
       }
+      //hvis balancen går i minus opdatere den ikke filen 
       else if(customerChange[0]<0){
          System.out.println("Du kan ikke komme i minus\n");
          wow = 1;
